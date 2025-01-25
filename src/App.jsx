7e7443bd23a6d35a80c0f6196188848ab1d17edc
SuriyaSR <suriyasun19@gmail.com>
2025-01-25 23:09:41 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        FAQ Accordion React Application
+      </div>
+    </>
+  )
+}
+
+export default App
